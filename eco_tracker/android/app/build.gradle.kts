33a85ec9e834074b7d1030168d481d0366c074af
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-03-23 17:23:24 +0000

    fix build.gradle for other devs

--- eco_tracker/android/app/build.gradle.kts
+++ eco_tracker/android/app/build.gradle.kts
@@ -44,18 +44,26 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+            if (keystorePropertiesFile.exists()) {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
 
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("release")
+            if (keystorePropertiesFile.exists()) {
+                signingConfig = signingConfigs.getByName("release")
+            } else {
+                // Use debug signing config if keystore properties are not available
+                signingConfig = signingConfigs.getByName("debug")
+            }
         }
     }
 }
