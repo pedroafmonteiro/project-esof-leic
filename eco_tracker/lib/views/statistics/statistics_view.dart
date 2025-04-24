788d44f1962e0e0ccbab650b06c5b886f00da3ad
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-04-24 11:59:57 +0100

    imported graphs package and implemented filter chips on statistics page

--- eco_tracker/lib/views/statistics/statistics_view.dart
+++ eco_tracker/lib/views/statistics/statistics_view.dart
@@ -1,11 +1,89 @@
 import 'package:eco_tracker/views/common/general_page.dart';
 import 'package:flutter/material.dart';
 
-class StatisticsView extends GeneralPage {
-  const StatisticsView({super.key}) : super(title: "Statistics", hasFAB: false);
+class StatisticsView extends StatefulWidget {
+  const StatisticsView({super.key});
+
+  @override
+  State<StatisticsView> createState() => _StatisticsViewState();
+}
+
+class _StatisticsViewState extends State<StatisticsView> {
+  String selectedFilter = "Daily";
+
+  @override
+  Widget build(BuildContext context) {
+    return StatisticsPage(
+      selectedFilter: selectedFilter,
+      onFilterChanged: (filter) {
+        setState(() {
+          selectedFilter = filter;
+        });
+      },
+    );
+  }
+}
+
+class StatisticsPage extends GeneralPage {
+  final String selectedFilter;
+  final void Function(String) onFilterChanged;
+
+  const StatisticsPage({
+    super.key,
+    required this.selectedFilter,
+    required this.onFilterChanged,
+  }) : super(title: "Statistics", hasFAB: false);
 
   @override
   Widget buildBody(BuildContext context) {
-    return Center(child: Text('To be implemented.'));
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FilterChip(
+              label: const Text("Daily"),
+              selected: selectedFilter == "Daily",
+              showCheckmark: false,
+              onSelected: (value) {
+                if (value) {
+                  onFilterChanged("Daily");
+                }
+              },
+            ),
+            FilterChip(
+              label: const Text("Weekly"),
+              selected: selectedFilter == "Weekly",
+              showCheckmark: false,
+              onSelected: (value) {
+                if (value) {
+                  onFilterChanged("Weekly");
+                }
+              },
+            ),
+            FilterChip(
+              label: const Text("Monthly"),
+              selected: selectedFilter == "Monthly",
+              showCheckmark: false,
+              onSelected: (value) {
+                if (value) {
+                  onFilterChanged("Monthly");
+                }
+              },
+            ),
+            FilterChip(
+              label: const Text("Yearly"),
+              selected: selectedFilter == "Yearly",
+              showCheckmark: false,
+              onSelected: (value) {
+                if (value) {
+                  onFilterChanged("Yearly");
+                }
+              },
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
